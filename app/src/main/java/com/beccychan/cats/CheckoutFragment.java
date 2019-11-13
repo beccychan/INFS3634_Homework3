e1760737e8b7885c34818748b4e8c2092b29bba4
beccychan <z5114764@ad.unsw.edu.au>
2019-11-13 22:40:12 +1100

    Update to layout

--- app/src/main/java/com/beccychan/cats/CheckoutFragment.java
+++ app/src/main/java/com/beccychan/cats/CheckoutFragment.java
@@ -34,7 +34,7 @@ public class CheckoutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_checkout, container, false);
+        View view = inflater.inflate(R.layout.fragment_favourites, container, false);
 
         checkoutList = load("orders", checkoutList);
         view = buildRecyclerView(view);
--- app/src/main/java/com/beccychan/cats/FavouritesAdapter.java
+++ app/src/main/java/com/beccychan/cats/FavouritesAdapter.java
@@ -38,7 +38,7 @@ public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesAdapter.Fa
     @NonNull
     @Override
     public FavouritesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.checkout_item, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.favourites_item, parent, false);
         return new FavouritesViewHolder(v, favouritesListener);
     }
 
